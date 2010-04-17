53fce13bf4d36a162a0bf53d230f0cbfaea4b132
F1ll3r <fabian.wiedenhoefer@gmx.de>
2010-04-17 21:24:51 +0200

    some changes in the userinterface

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -5,11 +5,11 @@
  *      Author: Max
  */
 
-//PENISGNIHIHI
 
 #include "UserInterface.h"
 #include "Game.h"
 #include "irrlicht.h"
+#include "Settings.h"
 
 UserInterface::UserInterface(Game* game) {
 	this->game = game;
@@ -54,8 +54,24 @@ void UserInterface::init(){
 				guienv->addStaticText(L"000",irr::core::rect<irr::s32>(300,30,500,50),
 						false,false,options,UI_GUI_Element_Grass_Text));
 	optionsElements.push_back(
-			guienv->addButton  (irr::core::rect<irr::s32>(290,320,400,340),
-					options,UI_GUI_Element_Resolution,L"Test",L" changes"));
+			guienv->addComboBox(irr::core::rect<irr::s32>(20,20,120,40),options,UI_GUI_Element_Resolution));
+			irr::gui::IGUIElement *a = options->getElementFromId(UI_GUI_Element_Resolution);
+			if(dynamic_cast<irr::gui::IGUIComboBox*>(a)){
+				irr::gui::IGUIComboBox* b = dynamic_cast<irr::gui::IGUIComboBox*>(a);
+				b->addItem(L"1024 x 768");	//0
+				b->addItem(L"1152 x 864"); 	//1
+				b->addItem(L"1280 x 720"); 	//2
+				b->addItem(L"1280 x 800"); 	//3
+				b->addItem(L"1440 x 900"); 	//4
+				b->addItem(L"1600 x 900"); 	//5
+				b->addItem(L"1280 x 960"); 	//6
+				b->addItem(L"1600 x 1200"); //7
+				b->addItem(L"1600 x 900"); 	//8
+				b->addItem(L"1920 x 1080"); //9
+				b->addItem(L"1920 x 1200"); //10
+			}
+			else My_Assert(0);
+
 	optionsElements.push_back(
 			guienv->addButton  (irr::core::rect<irr::s32>(290,320,380,340),
 					options,UI_GUI_Element_Apply,L"Apply",L"Apply changes"));
@@ -109,8 +125,7 @@ bool UserInterface::OnEvent(const irr::SEvent& event){
 				mainButtons[UI_Button_Main_Exit	  - 100]->setEnabled(false);
 				break;
 
-			case UI_GUI_Element_Apply:
-				break;
+
 			case UI_GUI_Element_Resolution:
 				break;
 			case UI_GUI_Element_Depth:
@@ -128,6 +143,25 @@ bool UserInterface::OnEvent(const irr::SEvent& event){
 			case UI_GUI_Element_Save:
 				// TODO: save
 				break;
+			case UI_GUI_Element_Apply:
+				/*Settings s;
+
+				irr::gui::IGUIElement *a = options->getElementFromId(UI_GUI_Element_Resolution);
+				if(dynamic_cast<irr::gui::IGUIComboBox*>(a)){
+					irr::gui::IGUIComboBox* b = dynamic_cast<irr::gui::IGUIComboBox*>(a);
+					switch(b->getSelected()){
+						case 0: //1024 x 768
+							//s.resolution = irr::core::dimension2du;
+
+							break;
+						default:
+
+							break;
+					}
+				}
+				game->setSettings(s);
+				*/
+
 
 			case UI_GUI_Element_Close:
 				drawOptions = false;
@@ -147,6 +181,7 @@ bool UserInterface::OnEvent(const irr::SEvent& event){
 	if(event.GUIEvent.EventType == irr::gui::EGET_SCROLL_BAR_CHANGED){
 		switch (event.GUIEvent.Caller->getID()) {
 		case UI_GUI_Element_Grass:
+
 			//irr::gui::* b = options->getElementFromId(UI_GUI_Element_Grass);
 			//irr::gui::IGUIElement* a = options->getElementFromId(UI_GUI_Element_Grass_Text);
 			//a->setText(b->getPos());
